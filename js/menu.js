3b8a1cca91811709fe59d27b92c38b9ae1ad3bb4
kadupl <kaduplopes2005@gmail.com>
2026-03-19 18:34:39 -0300

    desenvolvimento do portifolio em geral

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,20 @@
-const botao = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
+const track = document.querySelector('.track');
 
-botao.addEventListener('click', () => {
-  menu.classList.toggle('ativo');
-});
+track.innerHTML += track.innerHTML;
+
+let position = 0;
+let speed = 0.8; // pixels por frame
+
+function animate() {
+  const trackWidth = track.scrollWidth / 2;
+  position -= speed;
+
+  if (Math.abs(position) >= trackWidth) {
+    position = 0;
+  }
+
+  track.style.transform = `translateX(${position}px)`;
+  requestAnimationFrame(animate);
+}
+
+window.addEventListener('load', animate);
